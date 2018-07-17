db0ef7e146c3902452b6a76913f182ea89c1b889
bharat <vatsbharat@gmail.com>
2018-07-17 12:54:55 +0530

    New repo

---
+++ controllers/restaurant.controller.js
@@ -0,0 +1,11 @@
+var Restaurant = {
+    getRestaurantsList: function(req, res){
+        res.json({message: "Restaurant list"});
+    },
+
+    getRestaurantDetails: function(req, res){
+
+    }
+};
+
+module.exports = Restaurant;
