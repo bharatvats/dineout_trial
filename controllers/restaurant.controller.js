e55cbbf258d7881bfc08749fb611551658e7cee0
bharat <vatsbharat@gmail.com>
2018-07-18 17:25:56 +0530

    Add rest api

--- controllers/restaurant.controller.js
+++ controllers/restaurant.controller.js
@@ -62,6 +62,56 @@ class RestaurantController{
             res.status(200).json({code: 400, message: "Please select restaurant id"});
         }
     }
+    restaurantWithOffer(req, res){
+        if(req.params.restaurantId && req.params.offerId){
+            let {restaurantId, offerId} = req.params;
+            con.query(`SELECT r.id, r.name, r.features, r.contact, r.address, r.price, o.* FROM restaurants r 
+                LEFT JOIN offers o ON o.restaurant_id = r.id AND o.offer_id = ${offerId}
+                WHERE r.id= ${restaurantId}`, function(err, results, fields){
+                if(err){
+                    return res.status(200).json({code: 500, err: err});
+                }
+                else if(results.length){
+                    con.query(`SELECT rt.day, rt.opening_time, rt.closing_time FROM restaurant_timings rt 
+                    WHERE restaurant_id = ${restaurantId}`, function(err, timings, fields){
+                        if(err){
+                            return res.status(200).json({code: 500, err: err});
+                        }
+                        let restaurant = {};
+                        restaurant.id = results[0].id;
+                        restaurant.name = results[0].name;
+                        restaurant.address = results[0].address;
+                        restaurant.features = results[0].features;
+                        restaurant.contact = results[0].contact;
+                        restaurant.price = results[0].price;
+                        restaurant.id = results[0].id;
+                        restaurant.offers = [];
+                        restaurant.timings = timings;
+                        for(let result of results){
+                            if(result.offer_id){
+                                restaurant.offers.push({
+                                    offer_id: result.offer_id,
+                                    offer_name: result.offer_name,
+                                    discount: result.discount_val,
+                                    start_time: result.start_time,
+                                    end_time: result.end_time
+                                });
+                            }
+                        }
+                        return res.json({code: 200, data: restaurant});
+                    });
+                    
+                }
+                else{
+                    return res.json({code: 400, message: "No restaurant found"});    
+                }
+                
+            });
+        }
+        else{
+            res.status(200).json({code: 400, message: "Please select restaurant & offer id"});
+        }
+    }
 
 }
 module.exports = RestaurantController;
--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -4,4 +4,5 @@ var RestaurantController = require('./../controllers/restaurant.controller');
 var Restaurant = new RestaurantController();
 router.get('/list', Restaurant.RestaurantsList);
 router.get('/getDetails/:restaurantId', Restaurant.getRestaurantDetails);
+router.get('/restaurantWithOffer/:restaurantId/:offerId', Restaurant.restaurantWithOffer);
 module.exports = router;
