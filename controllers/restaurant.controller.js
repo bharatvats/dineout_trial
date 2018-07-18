04df64e32484996b812b467cd09896ccf56dfb55
bharat <vatsbharat@gmail.com>
2018-07-18 12:52:34 +0530

    restaurant apis

--- controllers/restaurant.controller.js
+++ controllers/restaurant.controller.js
@@ -6,7 +6,7 @@ class RestaurantController{
     }
 
     RestaurantsList(req, res){
-        con.query('SELECT id, name, features, contact, address, price FROM restaurants;', function(err, results, fields){
+        con.query('SELECT r.id, r.name, r.features, r.contact, r.address, r.price, count(o.restaurant_id) as offer_count FROM restaurants r LEFT JOIN offers o ON r.id = o.restaurant_id GROUP BY o.restaurant_id, r.id ORDER BY offer_count DESC;', function(err, results, fields){
             if(err){
                 return res.status(200).json({code: 500, err: err});
             }
@@ -15,11 +15,30 @@ class RestaurantController{
     }
     getRestaurantDetails(req, res){
         if(req.params.restaurantId){
-            con.query(`SELECT id, name, features, contact, address, price FROM restaurants WHERE id= ${req.params.restaurantId}`, function(err, results, fields){
+            con.query(`SELECT r.id, r.name, r.features, r.contact, r.address, r.price, o.* FROM restaurants r LEFT JOIN offers o ON o.restaurant_id = r.id WHERE r.id= ${req.params.restaurantId}`, function(err, results, fields){
                 if(err){
                     return res.status(200).json({code: 500, err: err});
                 }
-                res.json({code: 200, data: results});
+                console.log(results);
+                let restaurant = {};
+                
+                restaurant.id = results[0].id;
+                restaurant.name = results[0].name;
+                restaurant.features = results[0].features;
+                restaurant.contact = results[0].contact;
+                restaurant.price = results[0].price;
+                restaurant.id = results[0].id;
+                restaurant.offers = [];
+                for(let result of results){
+                    restaurant.offers.push({
+                        offer_id: result.offer_id,
+                        oofer_name: result.offer_name,
+                        discount: result.discount_val,
+                        start_time: result.start_time,
+                        end_time: result.end_time
+                    });
+                }
+                res.json({code: 200, data: restaurant});
             });
         }
         else{
@@ -28,29 +47,4 @@ class RestaurantController{
     }
 
 }
-// var Restaurant = {
-//     RestaurantsList: function(req, res){
-//         con.query('SELECT id, name, features, contact, address, price FROM restaurants;', function(err, results, fields){
-//             if(err){
-//                 return res.status(200).json({code: 500, err: err});
-//             }
-//             res.json({code: 200, data: results});
-//         });
-//     },
-
-//     getRestaurantDetails: function(req, res){
-//         if(req.params.restaurantId){
-//             con.query(`SELECT id, name, features, contact, address, price FROM restaurants WHERE id= ${req.params.restaurantId}`, function(err, results, fields){
-//                 if(err){
-//                     return res.status(200).json({code: 500, err: err});
-//                 }
-//                 res.json({code: 200, data: results});
-//             });
-//         }
-//         else{
-//             res.status(200).json({code: 400, message: "Please select restaurant id"});
-//         }
-//     }
-// };
-
 module.exports = RestaurantController;
--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var RestaurantController = require('./../controllers/restaurant.controller');
 var Restaurant = new RestaurantController();
-console.log(Restaurant);
 router.get('/list', Restaurant.RestaurantsList);
 router.get('/getDetails/:restaurantId', Restaurant.getRestaurantDetails);
 module.exports = router;
